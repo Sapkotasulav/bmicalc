5e9676b088aa82d46012e2c690c059567ef20130
LuShIfER33 <sulavsapkota33@gmail.com>
2024-07-24 21:19:53 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Hello from "./components/Hello";
+import World from "./components/World";
+import Input from "./components/Input";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='maincontainer'>
+      
+     
+      <form>
+        <div className='innercontainer'>
+          <div id='weight'><Hello /></div>
+          <div id='height'> <World /></div>
+          <div id='buttons'>
+            
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
---
+++ src/components/World.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const World = () => {
+  return (
+    <div>
+      fuck you Nepal
+    </div>
+  )
+}
+
+export default World
