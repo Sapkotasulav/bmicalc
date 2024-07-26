e9d62cfb6b2d48e6b16846b27d8fbf81406d8bed
LuShIfER33 <sulavsapkota33@gmail.com>
2024-07-26 14:15:10 +0545

    use of usestate()

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import Hello from "./components/Hello";
 import "./App.css";
+import { useState } from "react";
 function App() {
+  const [weight, setweight] = useState();
+  const [height, setheight] = useState();
+  const [BMI, setBMI] = useState("......");
+  const [message, setmessage] = useState("");
+  
+
   return (
     <div className="maincontainer">
       <Hello />
       <form>
         <div className="innercontainer">
           <div id="weight">
-            <input type="text" placeholder="Your Weight in KG" value={weight}></input>
+            {" "}
+            <h4>Weight:</h4>
+            <input
+              type="text"
+              placeholder="Your Weight in KG"
+              value={weight}
+              onChange={(e) => setweight(e.target.value)}
+            ></input>
           </div>
           <div id="height">
-            <input type="text" placeholder="Your Height in Inches" value={height}></input>
+            <h4>Height:</h4>
+            <input
+              type="text"
+              placeholder="Your Height in Inches"
+              value={height}
+              onChange={(e) => setheight(e.target.value)}
+            ></input>
           </div>
           <div id="buttons">
             <a href="#" class="button type--B">
               <div class="button__line"></div>
               <div class="button__line"></div>
               <span class="button__text">CALCULATE</span>
-              
             </a>
+          </div>
+          <div className="result">
+            <h4>
+              Your BMI is: {BMI}
+              <p>{message} </p>
+            </h4>
           </div>
         </div>
       </form>
